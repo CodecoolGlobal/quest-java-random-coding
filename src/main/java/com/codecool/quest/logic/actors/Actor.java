89950d3fc46ccc2679c30631109ac29e1878d9eb
Alex Szabo <szabbo87@gmail.com>
2020-04-07 13:59:36 +0200

    try

--- src/main/java/com/codecool/quest/logic/actors/Actor.java
+++ src/main/java/com/codecool/quest/logic/actors/Actor.java
@@ -15,7 +15,7 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (!nextCell.getTileName().equals("wall")) {
+        if (!nextCell.getTileName().equals("wall") && nextCell.getActor() == null) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
